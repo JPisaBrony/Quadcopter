53357956f4d3bc592b67afbcaf5151013a85f3a8
3rik <3rik.omni@gmail.com>
2013-10-23 15:45:04 -0500

    Fixed mistake in the TWI example

--- Code Samples/TWI_Read_Example/TWI_Read_Example.ino
+++ Code Samples/TWI_Read_Example/TWI_Read_Example.ino
@@ -1,3 +1,6 @@
+#include <avr/delay.h>
+#include <avr/interupt.h>
+
 void setup()
 {
   Serial.begin(9600);
@@ -15,10 +18,20 @@ ISR(TWI_vect)
   switch(statusCode)
   {
     case 0x08:
+    case 0x10:
       TWDR = 0x3Du;
+      TWCR &= ~0b00100000;
+    case 0x40:
+      break;
+    case 0x50:
+      Serial.print("Data: ");
+      Serial.println(TWDR);
+      _delay_ms(50);
     default:
+      Serial.print("Status: ");
       Serial.println(statusCode);
       _delay_ms(50);
   }
+  TWCR |= 0b10000000;
 }
 
