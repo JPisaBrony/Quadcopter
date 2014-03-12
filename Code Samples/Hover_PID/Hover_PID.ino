9b8e06604576c619f04ed0c6f3e98bf601232c69
3rik <3rik.omni@gmail.com>
2014-03-12 17:44:15 -0500

    Switched derivitive implementation to the gyroscope instead of finding the slope between two points. Finalized PID constants for yAxis

--- Code Samples/Hover_PID/Hover_PID.ino
+++ Code Samples/Hover_PID/Hover_PID.ino
@@ -3,14 +3,14 @@
 double X_PROPORTIONAL_GAIN;
 double X_INTEGRAL_GAIN;
 double X_DERIVATIVE_GAIN;
-#define Y_PROPORTIONAL_GAIN X_PROPORTIONAL_GAIN
-#define Y_INTEGRAL_GAIN X_INTEGRAL_GAIN
-#define Y_DERIVATIVE_GAIN X_DERIVATIVE_GAIN
+#define Y_PROPORTIONAL_GAIN 0.0374103943
+#define Y_INTEGRAL_GAIN 2.3828276645436E-5
+#define Y_DERIVATIVE_GAIN 25.1026401519
 
-double accel[3];
+double accel[3], gyro[3];
 double xAngleSP, yAngleSP, xAnglePV, yAnglePV, lastXAngleError, lastYAngleError, integralXAngleError, integralYAngleError, xCorrection, yCorrection;
 
-void updateCorrection(double accelerometerData[])
+void updateCorrection(double accelerometerData[], double gyroData[])
 {
   unsigned long time = timeDifferential;
   timeDifferential = 0;
@@ -20,19 +20,18 @@ void updateCorrection(double accelerometerData[])
   double yAngleError = yAnglePV - yAngleSP;
   double xP = X_PROPORTIONAL_GAIN * (xAngleError);
   double yP = Y_PROPORTIONAL_GAIN * (yAngleError);
-  double xDerivative = (xAngleError - lastXAngleError)/time;
-  double yDerivative = (yAngleError - lastYAngleError)/time;
+  double xDerivative = 1.5E-6 * gyro[1];
+  double yDerivative = 1.5E-6 * gyro[0];
   double xD = X_DERIVATIVE_GAIN * xDerivative;
-  double yD = Y_DERIVATIVE_GAIN * yDerivative;
-  integralXAngleError += (lastXAngleError + xAngleError) * (time / 2.0); //trapizoid rule
-  integralYAngleError += (lastYAngleError + yAngleError) * (time / 2.0);
+  double yD = Y_DERIVATIVE_GAIN * yDerivative;  
+  integralXAngleError += xAngleError * time;
+  integralYAngleError += yAngleError * time;
   double xI = X_INTEGRAL_GAIN * integralXAngleError;
   double yI = Y_INTEGRAL_GAIN * integralYAngleError;
-  lastXAngleError = xAngleError;
-  lastYAngleError = yAngleError;
   xCorrection = xP + xI + xD;
   yCorrection = yP + yI + yD;
 
+  /*
   Serial.print(time);
   Serial.print(',');
   Serial.print(yAnglePV, 5);
@@ -50,21 +49,14 @@ void updateCorrection(double accelerometerData[])
   Serial.print(yD, 5);
   Serial.print(',');
   Serial.print(yCorrection, 5);
+  */
 }
 
-void updateSetPointAndCorrection(double xAngle, double yAngle, double accelerometerData[])
+void updateSetPointAndCorrection(double xAngle, double yAngle, double accelerometerData[], double gyroData[])
 {
-  timeDifferential = 0;
   xAngleSP = xAngle;
   yAngleSP = yAngle;
-  xAnglePV = atan2(accelerometerData[0], accelerometerData[2]);
-  yAnglePV = atan2(accelerometerData[1], accelerometerData[2]);
-  double xAngleError = xAnglePV - xAngleSP;
-  double yAngleError = yAnglePV - yAngleSP;
-  lastXAngleError = xAngleError;
-  lastYAngleError = yAngleError;
-  xCorrection = X_PROPORTIONAL_GAIN * (xAngleError);
-  yCorrection = Y_PROPORTIONAL_GAIN * (yAngleError);
+  updateCorrection(accelerometerData, gyroData);
 }
 
 void setup()
@@ -80,32 +72,21 @@ void setup()
     if(digitalInput(_PIN7))
     {
       _delay_ms(5000);
+      timeDifferential = 0;//to prevent bad things from happening
       readI2CAccelerometer(accel);
-      updateSetPointAndCorrection(0, 0, accel);//set the starting point
+      updateSetPointAndCorrection(0, 0, accel, gyro);
       
       while(digitalInput(_PIN7))
-      {
-        //Potentiometers to addjust constants; Not for final quadcopter.
-        Y_PROPORTIONAL_GAIN = 0.0374103943;//((analogInput(_A0)/ 2.5)-1) / 16.0;
-        Y_INTEGRAL_GAIN = 4.765655329E-5;//((analogInput(_A0) / 5.0)) / 10000.0;
-        Y_DERIVATIVE_GAIN = 2.561;//analogInput(_A0) * 4;
-        
+      {        
         readI2CAccelerometer(accel);
-        
-        updateCorrection(accel);
-        Serial.print(',');
-        Serial.print(Y_PROPORTIONAL_GAIN, 10);
-        Serial.print(',');
-        Serial.print(Y_INTEGRAL_GAIN, 10);
-        Serial.print(',');
-        Serial.println(Y_DERIVATIVE_GAIN, 10);
-        
-        setMotors(0, 0, 0.5 + yCorrection, 0.5 - yCorrection);
+        readI2CGyroscope(gyro);
+        updateCorrection(accel, gyro);
+                
+        setMotors(0, 0, 0.75 + yCorrection, 0.75 - yCorrection);
         
       }
     }
     stopMotors();
-    //Serial.println("Flip the switch to start.");
   }
 }
 
