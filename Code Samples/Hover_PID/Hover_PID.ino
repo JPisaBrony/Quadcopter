e4a61949c8e49e033d84c1354c3923fc17b2092b
3rik <3rik.omni@gmail.com>
2014-04-01 15:37:27 -0500

    Warning! Lots of untested code!!! May or may not work properly!!! Added PID controller for headding (need to fix the fact that the magnetic compass can possible point 180 degrees of if the quadcopter is upside down.) Added PID controller for acceleration magnetude made some variables static inside methods Added a low pass filter for the data. (needs constants!!!)
    Most of this functionality is not implemented in the actual loop yet.

--- Code Samples/Hover_PID/Hover_PID.ino
+++ Code Samples/Hover_PID/Hover_PID.ino
@@ -3,33 +3,82 @@
 double X_PROPORTIONAL_GAIN;
 double X_INTEGRAL_GAIN;
 double X_DERIVATIVE_GAIN;
-#define Y_PROPORTIONAL_GAIN 0.0374103943
-#define Y_INTEGRAL_GAIN 2.3828276645436E-5
-#define Y_DERIVATIVE_GAIN 25.1026401519
+double Y_PROPORTIONAL_GAIN = 0.0374103943;
+double Y_INTEGRAL_GAIN = 2.3828276645436E-5;
+double Y_DERIVATIVE_GAIN = 25.1026401519;
+double HEADING_PROPORTIONAL_GAIN;
+double HEADING_INTEGRAL_GAIN;
+double HEADING_DERIVATIVE_GAIN;
+double ACCELERATION_PROPORTIONAL_GAIN;
+double ACCELERATION_INTEGRAL_GAIN;
+double ACCELERATION_DERIVATIVE_GAIN;
 
-double accel[3], gyro[3];
-double xAngleSP, yAngleSP, xAnglePV, yAnglePV, lastXAngleError, lastYAngleError, integralXAngleError, integralYAngleError, xCorrection, yCorrection;
+double ACCELEROMETER_LOW_PASS_FILTER_CONSTANT;
+double GYROSCOPE_LOW_PASS_FILTER_CONSTANT;
+double MAGNOMETER_LOW_PASS_FILTER_CONSTANT;
 
-void updateCorrection(double accelerometerData[], double gyroData[])
+
+double declination;
+double accelerationDueToGravity;
+
+double accel[3], gyro[3], mag[3], oldAccel[3], oldGyro[3], oldMag[3];
+
+double xAngleSP, yAngleSP, headingSP, accelerationSP;
+double xCorrection, yCorrection, hCorrection, aCorrection;
+
+void updateCorrection(double accelData[], double gyroData[], double magData[])
 {
   unsigned long time = timeDifferential;
   timeDifferential = 0;
-  xAnglePV = atan2(accelerometerData[0], accelerometerData[2]);
-  yAnglePV = atan2(accelerometerData[1], accelerometerData[2]);
+  static double xAnglePV, integralXAngleError;
+  static double yAnglePV, integralYAngleError;
+  static double headingPV, integralHeadingError;
+  static double accelerationPV, integralAccelerationError, previousAccelerationPV = 0;
+  
+  xAnglePV = atan2(accelData[0], accelData[2]);
+  yAnglePV = atan2(accelData[1], accelData[2]);
+  headingPV = atan2(-magData[1], magData[0]);
+  accelerationPV = sqrt(accelData[0] * accelData[0] + accelData[1] * accelData[1] + accelData[2] * accelData[2]);
+  
   double xAngleError = xAnglePV - xAngleSP;
   double yAngleError = yAnglePV - yAngleSP;
-  double xP = X_PROPORTIONAL_GAIN * (xAngleError);
-  double yP = Y_PROPORTIONAL_GAIN * (yAngleError);
-  double xDerivative = 1.5E-6 * gyro[1];
-  double yDerivative = 1.5E-6 * gyro[0];
+  double headingError = headingPV - headingSP;
+  double accelerationError = accelerationPV - accelerationSP;
+  
+  double xP = X_PROPORTIONAL_GAIN * xAngleError;
+  double yP = Y_PROPORTIONAL_GAIN * yAngleError;
+  double hP = HEADING_PROPORTIONAL_GAIN * headingError;
+  double aP = ACCELERATION_PROPORTIONAL_GAIN * accelerationError;
+  
+  double xDerivative = 1.5E-6 * gyroData[1];
+  double yDerivative = 1.5E-6 * gyroData[0];
+  double headingDerivative = 1.5E-6 * gyroData[2];
+  double accelerationDerivative;
+  if(time > 0)
+  {
+    accelerationDerivative = (accelerationPV - previousAccelerationPV) / time;
+    previousAccelerationPV = accelerationPV;
+  }
+  
   double xD = X_DERIVATIVE_GAIN * xDerivative;
-  double yD = Y_DERIVATIVE_GAIN * yDerivative;  
+  double yD = Y_DERIVATIVE_GAIN * yDerivative;
+  double hD = HEADING_DERIVATIVE_GAIN * headingDerivative;
+  double aD = ACCELERATION_DERIVATIVE_GAIN * accelerationDerivative;
+  
   integralXAngleError += xAngleError * time;
   integralYAngleError += yAngleError * time;
+  integralHeadingError += headingError * time;
+  integralAccelerationError += accelerationError * time;
+  
   double xI = X_INTEGRAL_GAIN * integralXAngleError;
   double yI = Y_INTEGRAL_GAIN * integralYAngleError;
+  double hI = HEADING_INTEGRAL_GAIN * integralHeadingError;
+  double aI = ACCELERATION_INTEGRAL_GAIN;
+  
   xCorrection = xP + xI + xD;
   yCorrection = yP + yI + yD;
+  hCorrection = hP + hI + hD;
+  aCorrection = aP + aI + aD;
 
   Serial.print(time);
   Serial.print(',');
@@ -56,11 +105,20 @@ void updateCorrection(double accelerometerData[], double gyroData[])
   Serial.println(X_DERIVATIVE_GAIN, 5);
 }
 
-void updateSetPointAndCorrection(double xAngle, double yAngle, double accelerometerData[], double gyroData[])
+void updateSetPointAndCorrection(double xAngle, double yAngle,  double heading, double acceleration, double accelData[], double gyroData[], double magData[])
 {
   xAngleSP = xAngle;
   yAngleSP = yAngle;
-  updateCorrection(accelerometerData, gyroData);
+  headingSP = heading + declination;
+  accelerationSP = acceleration + accelerationDueToGravity;
+  updateCorrection(accelData, gyroData, magData);
+}
+
+void lowPassFilter(double newData[], double oldData[], double lowPassConstant)
+{
+  oldData[0] = newData[0] = (newData[0] + lowPassConstant * oldData[0]) / (lowPassConstant + 1);
+  oldData[1] = newData[1] = (newData[1] + lowPassConstant * oldData[1]) / (lowPassConstant + 1);
+  oldData[2] = newData[2] = (newData[2] + lowPassConstant * oldData[2]) / (lowPassConstant + 1);
 }
 
 void setup()
@@ -79,7 +137,8 @@ void setup()
       timeDifferential = 0;//to prevent bad things from happening
       readI2CAccelerometer(accel);
       readI2CGyroscope(gyro);
-      updateSetPointAndCorrection(0, 0, accel, gyro);
+      readI2CCompass(mag);
+      updateSetPointAndCorrection(0, 0, 0, 0, accel, gyro, mag);
       
       while(digitalInput(_PIN7))
       {
@@ -91,7 +150,12 @@ void setup()
         
         readI2CAccelerometer(accel);
         readI2CGyroscope(gyro);
-        updateCorrection(accel, gyro);
+        readI2CCompass(mag);
+        lowPassFilter(accel, oldAccel, ACCELEROMETER_LOW_PASS_FILTER_CONSTANT);
+        lowPassFilter(gyro, oldGyro, GYROSCOPE_LOW_PASS_FILTER_CONSTANT);
+        lowPassFilter(mag, oldMag, MAGNOMETER_LOW_PASS_FILTER_CONSTANT);
+        
+        updateCorrection(accel, gyro, mag);
                 
         setMotors(0.5 - xCorrection, 0.5 + xCorrection, 0.5 + yCorrection, 0.5 - yCorrection);
         
